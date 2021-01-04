3f76162765981ce985b4c636460551b37d28eddf
lixavi <121849896+lixavi@users.noreply.github.com>
2021-01-04 11:30:34 +0530

    updated

--- video_processor.py
+++ video_processor.py
@@ -1,4 +1,15 @@
+import cv2
+from .object_detector import ObjectDetector
 
+class VideoProcessor:
+    def __init__(self, model_path, labels_path):
+        self.object_detector = ObjectDetector(model_path, labels_path)
+
+    def process_video(self, video_path, output_path=None):
+        cap = cv2.VideoCapture(video_path)
+        if not cap.isOpened():
+            print("Error: Couldn't open video file.")
+            return
 
         fps = cap.get(cv2.CAP_PROP_FPS)
         width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
