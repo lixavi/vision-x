c0e5d063e71181c361ffb85e9afd75f819f7c603
lixavi <121849896+lixavi@users.noreply.github.com>
2021-01-16 11:32:29 +0530

    updated

--- object_detector.py
+++ object_detector.py
@@ -20,17 +20,4 @@ class ObjectDetector:
         scores = detections['detection_scores'][0].numpy()
         return boxes, classes, scores
 
-    def draw_detections(self, image, boxes, classes, scores):
-        for i in range(len(scores)):
-            if scores[i] > 0.5:
-                ymin, xmin, ymax, xmax = boxes[i]
-                xmin = int(xmin * image.shape[1])
-                ymin = int(ymin * image.shape[0])
-                xmax = int(xmax * image.shape[1])
-                ymax = int(ymax * image.shape[0])
-                class_id = int(classes[i])
-                label = self.labels[class_id]
-                color = (0, 255, 0)  # Green color for bounding box
-                cv2.rectangle(image, (xmin, ymin), (xmax, ymax), color, 2)
-                cv2.putText(image, f'{label}: {scores[i]:.2f}', (xmin, ymin - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
-        return image
+
