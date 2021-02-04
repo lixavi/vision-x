d901fab03437be25c980f6eeb57f87777ecd71cd
lixavi <121849896+lixavi@users.noreply.github.com>
2021-02-04 11:34:48 +0530

    updated

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
 from src.processing.video_processor import VideoProcessor
 from src.processing.image_processor import ImageProcessor
 
-def main():
-    model_path = 'data/pretrained_models/model.pb'
-    labels_path = 'data/pretrained_models/labels.txt'
 
-    # Process image
-    image_path = 'data/images/sample.jpg'
-    output_image_path = 'output/sample_output.jpg'
-    image_processor = ImageProcessor(model_path, labels_path)
-    image_processor.process_image(image_path, output_image_path)
-
-if __name__ == "__main__":
-    main()
