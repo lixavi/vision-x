b90ff63ed8d13824178eee9432c348ff52f510ba
lixavi <121849896+lixavi@users.noreply.github.com>
2021-02-08 11:35:20 +0530

    updated

--- image_processor.py
+++ image_processor.py
@@ -5,12 +5,6 @@ class ImageProcessor:
     def __init__(self, model_path, labels_path):
         self.object_detector = ObjectDetector(model_path, labels_path)
 
-    def process_image(self, image_path, output_path=None):
-        # Load image
-        image = cv2.imread(image_path)
-        if image is None:
-            print("Error: Couldn't open image file.")
-            return
 
         # Perform object detection
         boxes, classes, scores = self.object_detector.detect_objects(image)
