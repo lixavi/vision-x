1ad93d8ff094b3f56ab6bfd4bd4ab951d6c491bf
lixavi <121849896+lixavi@users.noreply.github.com>
2021-02-19 11:37:57 +0530

    updated

--- detection/utils.py
+++ detection/utils.py
@@ -6,6 +6,3 @@ def resize_image(image, target_size=(300, 300)):
 def preprocess_image(image):
     return image / 255.0
 
-def postprocess_detections(boxes, classes, scores, confidence_threshold=0.5):
-    valid_indices = np.where(scores > confidence_threshold)[0]
-    return boxes[valid_indices], classes[valid_indices], scores[valid_indices]
