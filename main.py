f1eeaef220bfb12188a6b23ca521e22260de4e47
lixavi <121849896+lixavi@users.noreply.github.com>
2021-03-12 11:40:05 +0530

    updated

--- main.py
+++ main.py
@@ -5,11 +5,7 @@ def main():
     model_path = 'data/pretrained_models/model.pb'
     labels_path = 'data/pretrained_models/labels.txt'
 
-    # Process video
-    video_path = 'data/videos/sample.mp4'
-    output_video_path = 'output/sample_output.mp4'
-    video_processor = VideoProcessor(model_path, labels_path)
-    video_processor.process_video(video_path, output_video_path)
+
 
     # Process image
     image_path = 'data/images/sample.jpg'
