62830d0c4183f09a65903b6e3013f7bfe4baf052
lixavi <121849896+lixavi@users.noreply.github.com>
2021-04-09 21:28:26 +0530

    updated

--- object_detector.py
+++ object_detector.py
@@ -8,7 +8,9 @@ class ObjectDetector:
         self.labels = self.load_labels(labels_path)
         self.detect_fn = self.model.signatures['serving_default']
 
-
+    def load_labels(self, labels_path):
+        with open(labels_path, 'r') as f:
+            return [line.strip() for line in f.readlines()]
 
     def detect_objects(self, image):
         input_tensor = tf.convert_to_tensor(image)
