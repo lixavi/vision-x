c5bd9459dca754f966f515ac0ab6432f83e25a54
lixavi <121849896+lixavi@users.noreply.github.com>
2021-04-11 21:28:39 +0530

    updated

--- object_detector.py
+++ object_detector.py
@@ -12,13 +12,7 @@ class ObjectDetector:
         with open(labels_path, 'r') as f:
             return [line.strip() for line in f.readlines()]
 
-    def detect_objects(self, image):
-        input_tensor = tf.convert_to_tensor(image)
-        detections = self.detect_fn(input_tensor)
-        boxes = detections['detection_boxes'][0].numpy()
-        classes = detections['detection_classes'][0].numpy().astype(np.uint8)
-        scores = detections['detection_scores'][0].numpy()
-        return boxes, classes, scores
+
 
     def draw_detections(self, image, boxes, classes, scores):
         for i in range(len(scores)):
