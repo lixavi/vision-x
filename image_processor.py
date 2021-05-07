45aed5bece6d145316edee9ad7eea32217555467
lixavi <121849896+lixavi@users.noreply.github.com>
2021-05-07 21:38:12 +0530

    updated

--- image_processor.py
+++ image_processor.py
@@ -12,16 +12,4 @@ class ImageProcessor:
             print("Error: Couldn't open image file.")
             return
 
-        # Perform object detection
-        boxes, classes, scores = self.object_detector.detect_objects(image)
-        # Draw detection results on the image
-        self.object_detector.draw_detections(image, boxes, classes, scores)
 
-        # Display or save the processed image
-        if output_path:
-            cv2.imwrite(output_path, image)
-            print(f"Processed image saved to {output_path}")
-        else:
-            cv2.imshow('Object Detection', image)
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
