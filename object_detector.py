96aa9f0306d836f63d971c359787bbd4e83afab4
AA <121849896+lixavi@users.noreply.github.com>
2024-01-30 08:19:02 +0530

    Create object_detector.py

---
+++ object_detector.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+import cv2
+
+class ObjectDetector:
+    def __init__(self, model_path):
+        self.model = tf.saved_model.load(model_path)
+        self.detect_fn = self.model.signatures['serving_default']
+
+    def detect_objects(self, image):
+        input_tensor = tf.convert_to_tensor(image)
+        detections = self.detect_fn(input_tensor)
+        # Process detections
+        return detections
